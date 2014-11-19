19f1638549a43ca5c54addf843e098a7547a9106
Rosca Sergiu <rmagnum2002@gmail.com>
2014-11-19 19:25:23 +0200

    removed unused assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ set :deploy_to, '/home/rmagnum2002/apps/transager'
 
 # Default value for :scm is :git
 set :scm, :git
+set :rvm_type, :user
+set :rvm_ruby_version, '2.1.1@transager'
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -27,7 +29,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -51,7 +53,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'cache:clear'
+        # execute :rake, 'cache:clear'
       end
     end
   end
